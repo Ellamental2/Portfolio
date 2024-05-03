1805eb0eadd0fc89ac9b3ac48cc089aa3eb160de
Ellamental2 <Ellammalle_1994@hotmail.com>
2024-05-03 22:31:38 +0100

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import styles from "./Hero.module.css"
+import { getImageUrl } from "../../utils"
+
+export const Hero = () => {
+    return (
+        <section className={styles.container}>
+            <div className={styles.content}>
+                <h1 className={styles.title}>Hi, I&apos;m Ellam</h1>
+                <p className={styles.description}>I&apos;m a full stack developer with 2 years of experience using C#, ASP.Net and SQL. Reach out if you would like to learn more</p>
+                <a className={styles.contactBtn} href="mailto:ellam.fox@icloud.com">Contact Me</a>
+            </div>
+            <img
+                className={styles.heroImg}
+                src={getImageUrl("hero/heroImage.png")}
+                alt="hero-image"
+            />
+            <div className={styles.topBlur} />
+            <div className={styles.bottomBlur} />
+        </section>
+    )
+}
