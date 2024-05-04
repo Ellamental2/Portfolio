215ba471e56e48ee331ff70ae77189d974cfdda1
Ellamental2 <Ellammalle_1994@hotmail.com>
2024-05-04 13:10:28 +0100

    config changes

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/portfolio/",
   plugins: [react()],
   css: {
     modules: {
       localConvention: 'camelCase',
     }
-  },
-  base: "/Portfolio/"
+  }
 })
